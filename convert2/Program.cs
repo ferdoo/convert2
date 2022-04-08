7858e4017a4cd92ceefa54ed4b3a432dfd02ad30
Ferdo <50913838+ferdoo@users.noreply.github.com>
2022-04-08 18:35:12 +0200

    Add project files.

---
+++ convert2/Program.cs
@@ -0,0 +1,143 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.Design;
+using System.Linq;
+using System.Net;
+using System.Reflection;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace convert
+{
+    
+    class Program
+    {
+
+        public static string targetCurencyValue;
+        public static string exchaneUrl;
+
+
+        static void Main(string[] args)
+        {
+
+            if (args.Length == 0)
+            {
+                Console.WriteLine("Invalid args");
+                return;
+            }
+
+            
+            for (int i = 0; i < args.Count(); i++)
+            {
+                if (!TestArgument(args[i]))
+                {
+                    return;
+                }
+            }
+
+
+            var baseCurency = args[0];
+            var targetCurency = args[1];
+
+
+            Root myDeserializedClass = GetCurrency(baseCurency);
+
+            if (myDeserializedClass != null)
+            {
+                foreach (PropertyInfo prop in typeof(Rates).GetProperties())
+                {
+                    if (prop.Name == targetCurency)
+                    {
+                        targetCurencyValue = prop.GetValue(myDeserializedClass.rates, null).ToString();
+                        break;
+                    }
+
+                }
+
+
+                if (targetCurencyValue != "")
+                {
+                    Console.WriteLine();
+                    Console.WriteLine("********************************************");
+                    Console.WriteLine($"Course {myDeserializedClass.base_code} to {targetCurency} = {targetCurencyValue}");
+                    Console.WriteLine("Update on : " + myDeserializedClass.time_last_update_utc.ToLocalTime());
+                    Console.WriteLine("********************************************");
+                    Console.WriteLine();
+                }
+                else
+                {
+                    Console.WriteLine($"Error : Target Currency = {targetCurency} not exist.");
+                }
+
+            }
+            else
+            {
+                Console.WriteLine($"Error : Base Currency = {baseCurency} not exist. ");
+            }
+            
+            
+
+        }
+
+
+
+        /// <summary>
+        /// Ziskaj Konverzny list
+        /// </summary>
+        /// <param name="_baseCurency"></param>
+        /// <returns>Trieda Root</returns>
+        public static Root GetCurrency(string _baseCurency)
+        {
+
+            try
+            {
+                exchaneUrl = @"https://open.er-api.com/v6/latest/" + _baseCurency;
+                var json = new WebClient().DownloadString(exchaneUrl);
+
+
+                var jsonData = JObject.Parse(json);
+
+
+                Root myDeserializedClass = JsonConvert.DeserializeObject<Root>(json);
+
+                if (myDeserializedClass.result == "success")
+                {
+                    return myDeserializedClass;
+                }
+                else
+                {
+                    return null;
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Missing internet connection ? " + e);
+                return null;
+                
+            }
+
+        }
+
+
+
+
+        static bool TestArgument(string input)
+        {
+            string pattern = @"[A-Z]{3}";
+            Regex rg = new Regex(pattern);
+
+            if (!rg.IsMatch(input))
+            {
+                Console.WriteLine("Invalid args");
+                return false;
+            }
+
+            return true;
+
+        }
+    }
+}
+
---
+++ convert2/Root.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace convert
+{
+    public class Root
+    {
+        public string result { get; set; }
+        public string provider { get; set; }
+        public string documentation { get; set; }
+        public string terms_of_use { get; set; }
+        public int time_last_update_unix { get; set; }
+        public DateTime time_last_update_utc { get; set; }
+        public int time_next_update_unix { get; set; }
+        public DateTime time_next_update_utc { get; set; }
+        public int time_eol_unix { get; set; }
+        public string base_code { get; set; }
+        public Rates rates { get; set; }
+    }
+}
